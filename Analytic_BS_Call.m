34614167ca72308725b0316b575b77f39b84930a
DanSpi <spinelladaniele@gmail.com>
2016-09-19 13:13:23 +0200

    Add files via upload

---
+++ Analytic_BS_Call.m
@@ -0,0 +1,18 @@
+function [ Price ] = Analytic_BS_Call(S, K,t, T, sigma, r)
+%Computation of the analytical BS price of a Call option
+%
+%Input:
+%S Underlying stock price
+%K Strice price
+%t Actual time
+%T Maturity time 
+%sigma Standard deviation
+%r Risk free rate
+
+d_1=(log(S/K)+(r+0.5*sigma^2)*(T-t))/(sigma*sqrt(T-t));
+d_2=d_1-sigma*sqrt(T-t);
+
+Price=S*normcdf(d_1)-K*exp(-r*(T-t))*normcdf(d_2);
+
+end
+
